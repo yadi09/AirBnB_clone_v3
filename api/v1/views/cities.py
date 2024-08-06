8025bbe5d1a1088ae042106646bd1db640a5feab
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-06 15:34:54 -0700

    Task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,13 +8,15 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def cities(state_id):
     '''list of all city object of state object'''
     state = storage.get(State, state_id)
     if not state:
         abort(404)
     return jsonify([city.to_dict() for city in state.cities])
+
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def city(city_id):
@@ -23,6 +25,7 @@ def city(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -34,7 +37,8 @@ def delete_city(city_id):
     storage.save()
     return make_response(jsonify({}), 200)
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     '''create new city by srate id'''
     state = storage.get(State, state_id)
