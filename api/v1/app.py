0c4b0a44f7fbe82724f02a454c7e9de596830f95
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-06 14:31:28 -0700

    Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-'''application file contain Flask application API
+'''Contains a Flask web application API.
 '''
+import os
 from flask import Flask, jsonify
+
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
-'''The flask app created'''
-app.register_blueprint(app_views)
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    '''the flask teardown to close storage'''
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
+
 @app.errorhandler(404)
-def not_found(err):
-    '''handle not found 404 http error code'''
-    return jsonify({"error": "Not found"}), 404
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=app_host, port=app_port, threaded=True)
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
